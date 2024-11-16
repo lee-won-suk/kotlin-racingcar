b61e81eaf4c415e85a5e8d8df15a24131c4dc877
tjdjdjrdl123 <lee-won-suk@github.io>
2024-11-16 15:51:33 +0900

    feat: enum 사용하여 연산자 처리 로직 구현

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,56 @@
+import kotlin.math.floor
+
+enum class Calculator(val symbol: String) {
+    ADD("+") {
+        override fun execute(
+            deFaultNumber: Int,
+            plusNumber: Int,
+        ): Int {
+            return deFaultNumber + plusNumber
+        }
+    },
+    SUBSTRACT("-") {
+        override fun execute(
+            deFaultNumber: Int,
+            plusNumber: Int,
+        ): Int {
+            return deFaultNumber - plusNumber
+        }
+    },
+    MULTIPLY("*") {
+        override fun execute(
+            deFaultNumber: Int,
+            plusNumber: Int,
+        ): Int {
+            return deFaultNumber * plusNumber
+        }
+    },
+    DEVIDE("/") {
+        override fun execute(
+            deFaultNumber: Int,
+            plusNumber: Int,
+        ): Int {
+            return floor(deFaultNumber.toDouble() / plusNumber).toInt()
+        }
+    }, ;
+
+    companion object {
+        fun calculate(
+            calculation: String,
+            firstOperand: Int,
+            secondOperand: Int,
+        ): Int {
+            for (calculator in entries) {
+                if (calculator.symbol.equals(calculation)) {
+                    return calculator.execute(firstOperand, secondOperand)
+                }
+            }
+            throw IllegalArgumentException()
+        }
+    }
+
+    abstract fun execute(
+        deFaultNumber: Int,
+        plusNumber: Int,
+    ): Int
+}
