577df50020cdbfe13a7d877fe69a0748cdeac6dc
tjdjdjrdl123 <lee-won-suk@github.io>
2024-11-24 18:54:54 +0900

    refactor: 이름 잘못된 테스트클래스 수정 및 readMe 수정

--- src/test/kotlin/racingcar/CarsTest.kt
+++ src/test/kotlin/racingcar/CarsTest.kt
@@ -12,6 +12,14 @@ class CarsTest {
         val testCarList = listOf(Car("red",1),Car("blue",1))
         val testCars = Cars(testCarList)
         assertThat(cars).isEqualTo(testCars)
+    }
 
+    @Test
+    fun createWinningCarListTest()  {
+        val testCarList = listOf(Car("red",2),Car("blue",2),
+            Car("kaki",1))
+        val winningList = listOf(Car("red",2),Car("blue",2))
+        val cars = Cars(testCarList)
+        assertThat(cars.findWinner()).isEqualTo(winningList)
     }
 }
